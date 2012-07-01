a78d83d2e0311c3d7ac05637675a31987c1f9707
TingPing <tngpng@gmail.com>
2012-06-30 21:37:04 -0400

    merge changes from WDK

--- Main.cs
+++ Main.cs
@@ -1,3 +1,23 @@
+/* XChat Theme Manager
+ *
+ * Copyright (C) 2012 Patrick Griffs
+ * Copyright (C) 2012 Berke Viktor
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA
+ */
+
 using System;
 using System.Collections.Generic;
 using System.Collections;
@@ -5,11 +25,12 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.IO;
+/* using System.IO.Compression; */
+using System.IO.Packaging;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
 using System.Net;
-using Ionic.Zip;
 
 namespace thememan
 {
@@ -20,6 +41,8 @@ namespace thememan
 
         public string xchatdir;
         public string themedir = "themes\\";
+
+        OpenFileDialog importDialog;
 
         public XTM ()
 		{
@@ -34,18 +57,41 @@ namespace thememan
 			} else
 				Console.WriteLine("Install not found");
 
-            ShowFiles();
+            ListThemes();
+
+            String[] arguments = Environment.GetCommandLineArgs();
+            if (arguments.Length > 1)
+            {
+                FileInfo fi = new FileInfo(arguments[1]);
+                attemptImport(fi);
+            }
         }
 
-        private void ShowFiles()
+        private void ListThemes()
         {
-            if (System.IO.Directory.Exists(xchatdir + themedir))
+            themelist.Items.Clear();
+
+            if (Directory.Exists(xchatdir + themedir))
+            {
                 foreach (string theme in Directory.GetDirectories(xchatdir + themedir))
                 {
                     themelist.Items.Add(theme.Remove(0, xchatdir.Length + themedir.Length));
                 }
+            }
             else
+            {
                 Directory.CreateDirectory(xchatdir + themedir);
+            }
+
+            if (themelist.Items.Count == 0)
+            {
+                applybutton.Enabled = false;
+                deleteButton.Enabled = false;
+            }
+            else
+            {
+                themelist.SetSelected(0, true);
+            }
         }
 
         private void ShowColors(List<List<string>> themecolors)
@@ -100,32 +146,182 @@ namespace thememan
 
         private void applybutton_Click_1(object sender, EventArgs e)
         {
-            MessageBox.Show("XChat must be closed and this will overwrite your current theme, Continue?", "Warning", MessageBoxButtons.OKCancel, MessageBoxIcon.Warning);
-            File.Copy(xchatdir + themedir + themelist.SelectedItem.ToString() + "\\colors.conf", xchatdir + "colors.conf", true);
-            if (File.Exists(xchatdir + themedir + themelist.SelectedItem.ToString() + "\\pevents.conf"))
+            DialogResult result = MessageBox.Show("XChat must be closed and this will overwrite your current theme!\n\nDo you wish to continue?", "Warning", MessageBoxButtons.OKCancel, MessageBoxIcon.Warning);
+            if (result == DialogResult.OK)
             {
-                File.Copy(xchatdir + themedir + themelist.SelectedItem.ToString() + "\\pevents.conf", xchatdir + "pevents.conf", true);
+                File.Copy(xchatdir + themedir + themelist.SelectedItem.ToString() + "\\colors.conf", xchatdir + "colors.conf", true);
+                if (File.Exists(xchatdir + themedir + themelist.SelectedItem.ToString() + "\\pevents.conf"))
+                {
+                    File.Copy(xchatdir + themedir + themelist.SelectedItem.ToString() + "\\pevents.conf", xchatdir + "pevents.conf", true);
+                }
             }
         }
 
         private void theme_selected(object sender, EventArgs e)
         {
-            ShowColors(ReadTheme(themelist.SelectedItem.ToString()));
+            if (themelist.SelectedItem != null)
+            {
+                ShowColors(ReadTheme(themelist.SelectedItem.ToString()));
+                applybutton.Enabled = true;
+                deleteButton.Enabled = true;
+            }
         }
 
         private void importbutton_Click_1(object sender, EventArgs e)
         {
-            OpenFileDialog importdialog = new OpenFileDialog();
-            importdialog.Filter = "Zip Files (.zip)|*.zip";
-            importdialog.FilterIndex = 1;
-            importdialog.ShowDialog();
+            importDialog = new OpenFileDialog();
+            importDialog.Filter = "XChat Theme Files|*.xct";
+            importDialog.FilterIndex = 1;
+            importDialog.FileOk += new CancelEventHandler(importdialog_FileOk);
+            importDialog.ShowDialog();
+        }
+
+        private void importdialog_FileOk(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            FileInfo fi = new FileInfo(importDialog.FileName);
+            attemptImport(fi);
+        }
+
+        private void attemptImport(FileInfo fi)
+        {
+            string themeName = fi.Name.Remove(fi.Name.Length - fi.Extension.Length);
+            int result = extractTheme(fi);
+            ListThemes();
+            /* although a check is added to ListThemes(), this would still fail if the theme file was invalid or the theme is already installed */
+            switch (result)
+            {
+                case 0:
+                    themelist.SetSelected(themelist.FindStringExact(themeName), true);
+                    /* required for command line invoking */
+                    ShowColors(ReadTheme(themeName));
+                    break;
+                case 1:
+                    MessageBox.Show("This theme is already installed!", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    themelist.SetSelected(themelist.FindStringExact(themeName), true);
+                    /* required for command line invoking */
+                    ShowColors(ReadTheme(themeName));
+                    break;
+                case 2:
+                    MessageBox.Show("Invalid theme file!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    break;
+            }
+        }
+        /* gzip solution, not good enough coz we need multiple files
+         * 
+        public string extractTheme(FileInfo fi)
+        {
+            using (FileStream inFile = fi.OpenRead())
+            {
+                string themeName = fi.Name.Remove(fi.Name.Length - fi.Extension.Length);
+                string destFolder = xchatdir + themedir + themeName;
+
+                if (!Directory.Exists(destFolder))
+                {
+                    Directory.CreateDirectory(destFolder);
+                }
+
+                using (FileStream outFile = File.Create(destFolder + "\\colors.conf"))
+                {
+                    using (GZipStream Decompress = new GZipStream(inFile, CompressionMode.Decompress))
+                    {
+                        Decompress.CopyTo(outFile);
+                    }
+                }
+                return themeName;
+            }
+        }
+         */
+
+        /* using System.IO.Package:
+         * http://weblogs.asp.net/jgalloway/archive/2007/10/25/creating-zip-archives-in-net-without-an-external-library-like-sharpziplib.aspx
+         * [Content_Types].xml must be present for every zip file
+         */
+
+        private const long BUFFER_SIZE = 4096;
+
+        private int extractTheme(FileInfo zipFile)
+        {
+            string themeName = zipFile.Name.Remove(zipFile.Name.Length - zipFile.Extension.Length);
+            string destFolder = xchatdir + themedir + themeName;
+
             try
             {
-                ZipFile zip = ZipFile.Read(importdialog.FileName);
-                zip.ExtractAll(xchatdir + themedir);
-                ShowFiles();
+                using (Package zip = Package.Open(zipFile.FullName, FileMode.Open))
+                {
+                    PackagePartCollection parts = zip.GetParts();
+
+                    if (Directory.Exists(destFolder))
+                    {
+                        return 1;
+                    }
+                    else
+                    {
+                        Directory.CreateDirectory(destFolder);
+                    }
+
+                    foreach (PackagePart part in parts)
+                    {
+                        /* not sure what's this good for */
+                        /* String archive = part.Uri.ToString().Replace(@"/", @"\"); */
+                        String destFile = destFolder + part.Uri.ToString();
+
+                            using (FileStream outFileStream = new FileStream(destFile, FileMode.CreateNew, FileAccess.ReadWrite))
+                            {
+                                using (Stream inStream = part.GetStream())
+                                {
+                                    long bufferSize = inStream.Length < BUFFER_SIZE ? inStream.Length : BUFFER_SIZE;
+                                    byte[] buffer = new byte[bufferSize];
+                                    int bytesRead = 0;
+                                    long bytesWritten = 0;
+
+                                    while ((bytesRead = inStream.Read(buffer, 0, buffer.Length)) != 0)
+                                    {
+                                        outFileStream.Write(buffer, 0, bytesRead);
+                                        bytesWritten += bufferSize;
+                                    }
+                                }
+                            }
+
+
+                    }
+                }
             }
-            catch{ } //not the best fix
+            catch (System.IO.FileFormatException)
+            {
+                return 2;
+            }
+
+            if (IsDirectoryEmpty(destFolder))
+            {
+                Directory.Delete(destFolder);
+                return 2;
+            }
+            else
+            {
+                return 0;
+            }
         }
+
+        public bool IsDirectoryEmpty(string path)
+        {
+            return !Directory.EnumerateFileSystemEntries(path).Any();
+        }
+
+        private void deleteButton_Click(object sender, EventArgs e)
+        {
+            DialogResult result = MessageBox.Show("Are you sure you want to delete this theme from the theme repo?\n\nYour currently applied theme won't be affected.", "Warning", MessageBoxButtons.OKCancel, MessageBoxIcon.Warning);
+            if (result == DialogResult.OK)
+            {
+                Directory.Delete(xchatdir + themedir + themelist.SelectedItem.ToString(), true);
+                ListThemes();
+                if (themelist.Items.Count == 0)
+                {
+                    deleteButton.Enabled = false;
+                }
+            }
+        }
+
+
+
     }
 }
